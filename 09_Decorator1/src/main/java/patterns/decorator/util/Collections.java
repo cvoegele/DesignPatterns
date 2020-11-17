6be6c9cdb6601c353f6f06c4d680b08de6097ae0
Christoph Vögele <christoph.voegele@students.fhnw.ch>
2020-11-17 10:37:09 +0100

    add 09

---
+++ 09_Decorator1/src/main/java/patterns/decorator/util/Collections.java
@@ -0,0 +1,11 @@
+package patterns.decorator.util;
+
+import java.util.Collection;
+
+public class Collections {
+
+	public static <T> Collection<T> unmodifiableCollection(Collection<T> c) {
+		return null;
+	}
+
+}
