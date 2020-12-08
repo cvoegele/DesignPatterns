1ab72bef413ff4c839b81269e2bdaed45fd94785
Christoph Vögele <christoph.voegele@students.fhnw.ch>
2020-12-08 11:03:30 +0100

    add history and macro manager

--- 11_Command/src/main/java/logo/StdHistoryManager.java
+++ 11_Command/src/main/java/logo/StdHistoryManager.java
@@ -2,63 +2,81 @@ package logo;
 
 import logo.commands.turtle.TurtleCommand;
 
+import java.util.Stack;
+
 public class StdHistoryManager implements HistoryManager {
-	private final LogoInterpreter interpreter;
+    private final LogoInterpreter interpreter;
 
-	public StdHistoryManager(LogoInterpreter interpreter) {
-		this.interpreter = interpreter;
-	}
+    public StdHistoryManager(LogoInterpreter interpreter) {
+        this.interpreter = interpreter;
+        undoStack = new Stack<>();
+        redoStack = new Stack<>();
+    }
 
-	/**
-	 * Adds a command to the history. The invoker of the add method is responsible
-	 * to execute the command, method add only registers the command in the
-	 * history for a subsequent undo/redo call.
-	 */
-	@Override
-	public void addCommand(TurtleCommand command) {
-		// TODO implement registration of a new command
-	}
+    private final Stack<TurtleCommand> undoStack;
+    private final Stack<TurtleCommand> redoStack;
 
-	/**
-	 * Clears the history.
-	 */
-	@Override
-	public void clear() {
-		// TODO implement clearing of the command list
-	}
+    /**
+     * Adds a command to the history. The invoker of the add method is responsible
+     * to execute the command, method add only registers the command in the
+     * history for a subsequent undo/redo call.
+     */
+    @Override
+    public void addCommand(TurtleCommand command) {
+        undoStack.add(command);
+        redoStack.clear();
+    }
 
-	/**
-	 * Performs an undo operation with the effect, that method getCommand returns
-	 * a list which does not contain the latest operation. If an undo is not
-	 * possible as the list of commands is empty, a message should be printed on
-	 * the console. Method undo is also responsible to actualize the window by
-	 * invoking interpreter.repaint().
-	 */
-	@Override
-	public void undo() {
-		// TODO update public view of command list as provided by getCommands and
-		// initiate redrawing of entire graphics by calling interpreter.repaint()
-	}
+    /**
+     * Clears the history.
+     */
+    @Override
+    public void clear() {
+        undoStack.clear();
+        redoStack.clear();
+    }
 
-	/**
-	 * Restores the least recently undone operation. If a redo is not possible as
-	 * no pending undone commands are available, a message should be printed on
-	 * the console. Method redo is also responsible to actualize the window by
-	 * executing the command to redo.
-	 */
-	@Override
-	public void redo() {
-		// TODO update public view, provided through getCommands and execute command
-		// to redo
-	}
+    /**
+     * Performs an undo operation with the effect, that method getCommand returns
+     * a list which does not contain the latest operation. If an undo is not
+     * possible as the list of commands is empty, a message should be printed on
+     * the console. Method undo is also responsible to actualize the window by
+     * invoking interpreter.repaint().
+     */
+    @Override
+    public void undo() {
+        // initiate redrawing of entire graphics by calling interpreter.repaint()
+        if (!undoStack.isEmpty()) {
+            redoStack.add(undoStack.pop());
+            interpreter.repaint();
+        } else {
+            System.out.println("No Commands in Undo Stack");
+        }
+    }
 
-	/**
-	 * Returns all commands which have been registered. This method is used by
-	 * method LogoInterpreter.repaint().
-	 */
-	@Override
-	public Iterable<TurtleCommand> getCommands() {
-		// TODO return a list of commands to be executed to redraw the current state
-		return null;
-	}
+    /**
+     * Restores the least recently undone operation. If a redo is not possible as
+     * no pending undone commands are available, a message should be printed on
+     * the console. Method redo is also responsible to actualize the window by
+     * executing the command to redo.
+     */
+    @Override
+    public void redo() {
+        if (!redoStack.isEmpty()){
+            var redo = redoStack.pop();
+            redo.execute();
+            undoStack.add(redo);
+        } else {
+            System.out.println("No Commands in Redo Stack");
+        }
+    }
+
+    /**
+     * Returns all commands which have been registered. This method is used by
+     * method LogoInterpreter.repaint().
+     */
+    @Override
+    public Iterable<TurtleCommand> getCommands() {
+        return undoStack;
+    }
 }
--- 11_Command/src/main/java/logo/StdMacroManager.java
+++ 11_Command/src/main/java/logo/StdMacroManager.java
@@ -1,59 +1,75 @@
 package logo;
 
 import logo.commands.Command;
+import logo.commands.control.NotFoundCommand;
+import logo.commands.turtle.CompositeCommand;
 import logo.commands.turtle.TurtleCommand;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StdMacroManager implements MacroManager {
-	private final LogoInterpreter interpreter;
+    private final LogoInterpreter interpreter;
 
-	public StdMacroManager(LogoInterpreter interpreter) {
-		this.interpreter = interpreter;
-	}
+    public StdMacroManager(LogoInterpreter interpreter) {
+        this.interpreter = interpreter;
+        macros = new HashMap<>();
+    }
 
-	/**
-	 * Indicates whether a macro is currently being recorded.
-	 */
-	@Override
-	public boolean isRecordingMacro() {
-		// TODO return the respective state value
-		return false;
-	}
+    private final Map<String, CompositeCommand> macros;
+    private boolean isRecording = false;
+    private CompositeCommand recording;
 
-	/**
-	 * Adds a command to the current macro. If this method is invoked while no
-	 * macro is being recorded, then a NullPointerException should be thrown.
-	 */
-	@Override
-	public void addCommand(TurtleCommand command) {
-		// TODO store the command provided by the parameter in the list of commands
-		// to be executed by the macro
-	}
+    /**
+     * Indicates whether a macro is currently being recorded.
+     */
+    @Override
+    public boolean isRecordingMacro() {
+        return isRecording;
+    }
 
-	/**
-	 * Starts the recording of a new macro.
-	 */
-	@Override
-	public void startMacro(String name) {
-		// TODO generate a new data structure to store a sequence of TurtleCommands
-		// as macro under the given name
-	}
+    /**
+     * Adds a command to the current macro. If this method is invoked while no
+     * macro is being recorded, then a NullPointerException should be thrown.
+     */
+    @Override
+    public void addCommand(TurtleCommand command) {
+        // TODO store the command provided by the parameter in the list of commands
+        if (!isRecording) throw new NullPointerException("No macro is being recorded");
+        recording.add(command);
+    }
 
-	/**
-	 * Closes the recording of the current macro and registers the macro under its
-	 * name.
-	 */
-	@Override
-	public void endMacro() {
-		// TODO store the recorded list of commands for instance in a map
-	}
+    /**
+     * Starts the recording of a new macro.
+     */
+    @Override
+    public void startMacro(String name) {
+        // TODO generate a new data structure to store a sequence of TurtleCommands
+        isRecording = true;
+        recording = new CompositeCommand(name);
+    }
 
-	/**
-	 * Returns the macro command with a given name. If no macro can be found with
-	 * this name, then a NotFoundCommand is returned.
-	 */
-	@Override
-	public Command getCommand(String name) {
-		// TODO retrieve the macro with the name provided from the store (e.g. a map)
-		return null;
-	}
+    /**
+     * Closes the recording of the current macro and registers the macro under its
+     * name.
+     */
+    @Override
+    public void endMacro() {
+        isRecording = false;
+        macros.put(recording.getName(), recording);
+    }
+
+    /**
+     * Returns the macro command with a given name. If no macro can be found with
+     * this name, then a NotFoundCommand is returned.
+     */
+    @Override
+    public Command getCommand(String name) {
+        // TODO retrieve the macro with the name provided from the store (e.g. a map)
+        if (!macros.containsKey(name)) {
+            return macros.get(name);
+        } else {
+            return new NotFoundCommand(name);
+        }
+    }
 }
